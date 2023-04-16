c19a038118cb2d93da17f76186ec3f89050c5b4d
JDay2 <jacobday4412@gmail.com>
2023-04-16 15:13:21 -0400

    hmmm

--- facultyMainPage_CTRL.cpp
+++ facultyMainPage_CTRL.cpp
@@ -25,6 +25,7 @@ class facultyMainPage
 
 void facultyMainPage::printOptions(user* passedUser){
 
+<<<<<<< HEAD
   user* current=passedUser;
   bool notSelected = true;
   int pass=0;
@@ -43,6 +44,46 @@ void facultyMainPage::printOptions(user* passedUser){
   callAppropriate(pass,current); 
 
 
+=======
+  current=user*;
+  bool logoutNotSelected = true;
+  
+  // loops infinitely until a user selects logout, ensuring user is able to perform as many actions as they want during a session
+  while(logoutNotSelected == true)
+  {
+    cout<<"Welcome "<<current->getfname()<<" to the student registration system!"<<endl;
+    cout<<"Faculty Main Page"<<endl;
+
+    cout<<"1. View Current Courses"<<endl;
+    cout<<"2. View Previous Courses"<<endl;
+    cout<<"3. View Account"<<endl;
+    cout<<"4. Logout"<<endl;
+    
+    int pass = 0;
+    bool validSelection = false; 
+    // loops to check to ensure valid user input
+    while(validSelection == false)
+    {
+      cout<<"Enter selection 1-4:";
+      cin>>pass;
+
+      if(pass == 1 || pass == 2 || pass == 3 || pass == 4)
+      {
+        validSelection == true;
+        callAppropriate(pass);
+        if(pass == 4)
+        {
+          logoutNotSelected = false;
+        }
+      } 
+     
+      else
+      {
+        cout << "Invalid input, please enter a value 1-4." << endl;
+      }
+    }
+  }
+>>>>>>> 5db600a957551e32ab4bd6ba3e993a0a65734fc9
 }
 
 
